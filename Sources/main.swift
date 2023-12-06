fdeba82efe1795d6cb247e2bdde5aaf691e996c9
27labs <132381958+27labs@users.noreply.github.com>
2023-12-06 01:44:36 +0000

    Terminar solución

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,9 +4,27 @@
 var entrada: Array<Array<String>> = []
 
 while let input = readLine() {
-    let numbers = input.split(separator: "").filter {["0","1","2","3","4","5","6","7","8","9"].contains( $0 )}
-    entrada.append( [String(numbers.first ?? "0"), String(numbers.last ?? "0")] )
+    var stepped = ""
+    for character in input.split(separator: "") {
+        stepped += character
+        stepped.replace("one", with: "1e")
+        stepped.replace("two", with: "2o")
+        stepped.replace("three", with: "3e")
+        stepped.replace("four", with: "4r")
+        stepped.replace("five", with: "5e")
+        stepped.replace("six", with: "6x")
+        stepped.replace("seven", with: "7n")
+        stepped.replace("eight", with: "8t")
+        stepped.replace("nine", with: "9e")
+    }
+
+    // print(stepped)
+    
+    let numbers = stepped.filter {["1","2","3","4","5","6","7","8","9"].contains( $0 )}
+    entrada.append( [String(numbers.first ?? " "), String(numbers.last ?? " ")] )
 }
+
+// print(entrada)
 
 var salida: Int = 0
 
