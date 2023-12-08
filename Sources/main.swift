3231fdb101933aa12a8b170a41010b043fdd53bf
27labs <132381958+27labs@users.noreply.github.com>
2023-12-08 00:33:54 +0000

    Terminar solución

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-var symbolHitboxesXY: Set<String> = []
 var numberHitboxesXY: [String:String] = [:]
+var starsXY: [String:[String]] = [:]
 
 var lineIndex = 0
 
@@ -14,18 +14,10 @@ while let input = readLine() {
     var coordenadas: Set<String> = []
     var numero = ""
     for character in splitted {
-        if !["0","1","2","3","4","5","6","7","8","9","."].contains(character) {
-             symbolHitboxesXY.update(with: "\(index-1) \(lineIndex-1)")
-             symbolHitboxesXY.update(with: "\(index) \(lineIndex-1)")
-             symbolHitboxesXY.update(with: "\(index+1) \(lineIndex-1)")
-             symbolHitboxesXY.update(with: "\(index-1) \(lineIndex)")
-             symbolHitboxesXY.update(with: "\(index+1) \(lineIndex)")
-             symbolHitboxesXY.update(with: "\(index-1) \(lineIndex+1)")
-             symbolHitboxesXY.update(with: "\(index) \(lineIndex+1)")
-             symbolHitboxesXY.update(with: "\(index+1) \(lineIndex+1)")
+        if !["0","1","2","3","4","5","6","7","8","9",".","*"].contains(character) {
              numero = ""
              coordenadas = []
-        } else if character != "." {
+        } else if ![".","*"].contains(character) {
             coordenadas.update(with: "\(index) \(lineIndex)")
             if numero == "" {
                 numero += "\(ocurrence) "
@@ -35,6 +27,19 @@ while let input = readLine() {
             for par in coordenadas {
                 numberHitboxesXY[par]=numero
             }
+        } else if character == "*" {
+            // print("*")
+            starsXY["\(index) \(lineIndex)"] = [
+                "\(index-1) \(lineIndex-1)", 
+                "\(index) \(lineIndex-1)", "\(index+1) \(lineIndex-1)", 
+                "\(index-1) \(lineIndex)",
+                "\(index+1) \(lineIndex)",
+                "\(index-1) \(lineIndex+1)",
+                "\(index) \(lineIndex+1)",
+                "\(index+1) \(lineIndex+1)"
+                ]
+            numero=""
+            coordenadas=[]
         } else {
             numero=""
             coordenadas=[]
@@ -46,8 +51,33 @@ while let input = readLine() {
 
 var numeros: Set<String> = []
 
-for hitbox in symbolHitboxesXY {
-    numeros.update(with: numberHitboxesXY[hitbox] ?? "0 0")
+// print(starsXY)
+
+// print("pre")
+
+for star in starsXY {
+    // print("in")
+    let hitbox = Set(star.value)
+    let posibles = hitbox.intersection(numberHitboxesXY.keys)
+    // print(posibles)
+    var numbers: Set<String> = []
+    for box in posibles {
+        numbers.update(with: numberHitboxesXY[box] ?? "0 0")
+    }
+    if numbers.count == 2 {
+        let gearValues = Array(numbers)
+
+        let id = gearValues[0].split(separator: " ")[0]
+
+        let numero1 = Int(gearValues[0].split(separator: " ")[1]) ?? 1
+        
+        let numero2 = Int(gearValues[1].split(separator: " ")[1]) ?? 1
+
+        numeros.update(with: "\(id) \(numero1 * numero2)")
+    }
+    // print("out")
 }
+
+// print("post")
 
 print(numeros.reduce(0, {m, n in m + (Int(n.split(separator: " ")[1]) ?? 0)}))
