a4981fb7f336fa849f8f6f32fe3f793aedc35839
27labs <132381958+27labs@users.noreply.github.com>
2023-12-08 02:37:44 +0000

    Terminar solución

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-var entrada: [Int] = []
+var entrada: [Int:Int] = [:]
+
+var indice = 1
 
 while let input = readLine() {
+    entrada[indice] = 1 + (entrada[indice] ?? 0)
+    indice += 1
     let cardNumbers = input.split(separator: ":")[1].split(separator: "|")
 
     let winning = cardNumbers[0].split(separator: " ")
@@ -19,9 +23,12 @@ while let input = readLine() {
     }
 
     if matches > 0 {
-        let value = 1 << (matches - 1)
-        entrada.append(value)
+        for _ in 1...(entrada[indice-1] ?? 1) {
+            for cardIndex in indice...(indice+matches-1) {
+                entrada[cardIndex] = 1 + (entrada[cardIndex] ?? 0)
+            }
+        }
     }
 }
 
-print(entrada.reduce(0, {m, n in m + n}))
+print(entrada.sorted(by: { $0.key < $1.key }).prefix(indice-1).reduce(0, {m, n in m + n.value}))
