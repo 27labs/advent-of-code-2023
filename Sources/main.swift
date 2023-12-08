65c76e0fc93473cc00dacab056246a38b45b8b66
27labs <132381958+27labs@users.noreply.github.com>
2023-12-08 01:24:59 +0000

    Terminar solución

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+var entrada: [Int] = []
+
+while let input = readLine() {
+    let cardNumbers = input.split(separator: ":")[1].split(separator: "|")
+
+    let winning = cardNumbers[0].split(separator: " ")
+
+    let scratched = cardNumbers[1].split(separator: " ")
+
+    var matches = 0
+
+    for number in scratched {
+        if winning.contains(number) {
+            matches += 1
+        }
+    }
+
+    if matches > 0 {
+        let value = 1 << (matches - 1)
+        entrada.append(value)
+    }
+}
+
+print(entrada.reduce(0, {m, n in m + n}))
