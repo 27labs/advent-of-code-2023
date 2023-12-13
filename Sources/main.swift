86193392acbe6c0eb9196d3195dec1f9b9132297
27labs <132381958+27labs@users.noreply.github.com>
2023-12-13 01:30:01 +0000

    Terminar solución

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,31 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+var entrada: [[Int]] = []
+
+while let input = readLine() {
+    entrada.append((input.split(separator: " ").map {
+        Int($0)!
+    }))
+}
+
+func extrapolate(history: [Int]) -> Int {
+    if (history.filter {
+        $0 != 0
+    }).count > 0 {
+        var difference: [Int] = []
+        for i in 1..<history.count {
+            difference.append(history[i] - history[i - 1])
+        }
+        return difference.last! + extrapolate(history: difference)
+    }
+    return 0
+}
+
+var sumOfValues = 0
+
+for history in entrada {
+    sumOfValues += history.last! + extrapolate(history: history)
+}
+
+print(sumOfValues)
